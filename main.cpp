d7cfa2d76d29a178f08af36239b1e9b2a01700b1
Tailwhip <msadowskizdunek@gmail.com>
2021-02-21 18:49:34 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+
+#include <adder.h>
+
+int main(){
+
+    printf("Greetings to your mom!\n");
+
+    std::cout << add(43.2, 53.5) << std::endl;
+    
+    return 0;
+}
